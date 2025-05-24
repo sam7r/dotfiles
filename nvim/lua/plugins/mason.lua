8f7d6c169dd98b5893002465093cd6d733c0d01f
sam <design@sjroberton.co.uk>
2025-05-24 13:00:34 +0100

    mason fix

--- nvim/lua/plugins/mason.lua
+++ nvim/lua/plugins/mason.lua
@@ -20,4 +20,8 @@ return {
             },
         },
     },
+    {
+        { "mason-org/mason.nvim", version = "^1.0.0" },
+        { "mason-org/mason-lspconfig.nvim", version = "^1.0.0" },
+    },
 }

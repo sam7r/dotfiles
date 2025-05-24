1341fdf6df83c2829a3843aa241711a8ff9759a7
sam <design@sjroberton.co.uk>
2025-05-24 12:59:48 +0100

    center nvim cmd

---
+++ nvim/lua/plugins/noice.lua
@@ -0,0 +1,44 @@
+return {
+    "folke/noice.nvim",
+    opts = function(_, opts)
+        opts.presets = {
+            command_palette = {
+                views = {
+                    cmdline_popup = {
+                        position = {
+                            row = "50%",
+                            col = "50%",
+                        },
+                        size = {
+                            min_width = 60,
+                            width = "auto",
+                            height = "auto",
+                        },
+                    },
+                    popupmenu = {
+                        relative = "editor",
+                        position = {
+                            row = 23,
+                            col = "50%",
+                        },
+                        size = {
+                            width = 60,
+                            height = "auto",
+                            max_height = 15,
+                        },
+                        border = {
+                            style = "rounded",
+                            padding = { 0, 1 },
+                        },
+                        win_options = {
+                            winhighlight = { Normal = "Normal", FloatBorder = "NoiceCmdlinePopupBorder" },
+                        },
+                    },
+                },
+            },
+        }
+        opts.lsp.signature = {
+            opts = { size = { max_height = 15 } },
+        }
+    end,
+}

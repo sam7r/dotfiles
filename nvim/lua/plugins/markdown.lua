8f45189d5f613d959d63b130e68b819bfa6baaf6
sam <design@sjroberton.co.uk>
2025-05-28 19:07:27 +0100

    setup codecompanion

--- nvim/lua/plugins/markdown.lua
+++ nvim/lua/plugins/markdown.lua
@@ -4,6 +4,12 @@ return {
         dependencies = { "nvim-treesitter/nvim-treesitter", "echasnovski/mini.icons" }, -- if you use standalone mini plugins
         ---@module 'render-markdown'
         ---@type render.md.UserConfig
-        opts = {},
+        ft = { "markdown", "codecompanion" },
+        opts = {
+            render_modes = true, -- Render in ALL modes
+            sign = {
+                enabled = false, -- Turn off in the status column
+            },
+        },
     },
 }

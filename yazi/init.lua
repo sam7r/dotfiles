6e4e70cb626dfbad200de83d2b5bb11b9ff78bed
sam <design@sjroberton.co.uk>
2025-10-17 12:13:32 +0100

    update yazi config

--- yazi/init.lua
+++ yazi/init.lua
@@ -1,28 +1,16 @@
 require("starship"):setup()
-require("full-border"):setup()
 
 require("smart-enter"):setup({
 	open_multi = true,
 })
 
-function Linemode:size_and_mtime()
-	local time = math.floor(self._file.cha.mtime or 0)
-	if time == 0 then
-		time = ""
-	elseif os.date("%Y", time) == os.date("%Y") then
-		time = os.date("%b %d %H:%M", time)
-	else
-		time = os.date("%b %d  %Y", time)
-	end
-
-	local size = self._file:size()
-	return string.format("%s %s", size and ya.readable_size(size) or "-", time)
-end
+require("recycle-bin"):setup()
 
 require("full-border"):setup({
 	-- Available values: ui.Border.PLAIN, ui.Border.ROUNDED
 	type = ui.Border.ROUNDED,
 })
+
 require("yatline"):setup({
 	section_separator = { open = "", close = "" },
 	part_separator = { open = "", close = "" },
@@ -115,3 +103,30 @@ local tokyo_night_theme = require("yatline-tokyo-night"):setup("night")
 require("yatline"):setup({
 	theme = tokyo_night_theme,
 })
+
+-- Cache current year to avoid repeated os.date calls
+local current_year = os.date("%Y")
+local last_year_check = os.time()
+
+function Linemode:size_and_mtime()
+	local time = math.floor(self._file.cha.mtime or 0)
+	if time == 0 then
+		time = ""
+	else
+		-- Refresh cached year once per minute
+		local now = os.time()
+		if now - last_year_check > 60 then
+			current_year = os.date("%Y")
+			last_year_check = now
+		end
+
+		if os.date("%Y", time) == current_year then
+			time = os.date("%b %d %H:%M", time)
+		else
+			time = os.date("%b %d  %Y", time)
+		end
+	end
+
+	local size = self._file:size()
+	return string.format("%s %s", size and ya.readable_size(size) or "-", time)
+end

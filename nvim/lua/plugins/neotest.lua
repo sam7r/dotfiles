27ffeb1582b37baac6d76fb5599d612f93368153
sam <design@sjroberton.co.uk>
2026-01-03 20:33:07 +0000

    update neotest config

--- nvim/lua/plugins/neotest.lua
+++ nvim/lua/plugins/neotest.lua
@@ -7,30 +7,54 @@ return {
             "nvim-lua/plenary.nvim",
             "antoinemadec/FixCursorHold.nvim",
             "nvim-treesitter/nvim-treesitter",
-
             {
                 "fredrikaverpil/neotest-golang", -- Installation
                 dependencies = {
                     "uga-rosa/utf8.nvim", -- Additional dependency required
                 },
             },
-            {
-                "nvim-neotest/neotest-jest",
-            },
-            {
-                "adrigzr/neotest-mocha",
-            },
         },
+        ---@class neotest.CoreConfig
         opts = {
+            -- See all config options with :h neotest.Config
+            discovery = {
+                -- Drastically improve performance in ginormous projects by
+                -- only AST-parsing the currently opened buffer.
+                enabled = false,
+                -- Number of workers to parse files concurrently.
+                -- A value of 0 automatically assigns number based on CPU.
+                -- Set to 1 if experiencing lag.
+                concurrent = 0,
+            },
+            running = {
+                -- Run tests concurrently when an adapter provides multiple commands to run.
+                concurrent = true,
+            },
+            summary = {
+                -- Enable/disable animation of icons.
+                animated = true,
+            },
             adapters = {
                 ["neotest-golang"] = {
                     -- Here we can set options for neotest-golang, e.g.
                     go_test_args = { "-timeout=60s" },
                     dap_go_enabled = true, -- requires leoluz/nvim-dap-go
                     sanitize_output = true,
-                    log_level = vim.log.levels.ERROR,
-                    runner = "gotestsum", -- needed at the moment, running project tests results in warn toast hell
+                    -- log_level = vim.log.levels.ERROR,
+                    -- runner = "gotestsum",
                 },
+            },
+        },
+    },
+    {
+        "nvim-neotest/neotest",
+        optional = true,
+        dependencies = {
+            "nvim-neotest/neotest-jest",
+            "adrigzr/neotest-mocha",
+        },
+        opts = {
+            adapters = {
                 ["neotest-jest"] = {
                     jestCommand = "npm test --",
                     jest_test_discovery = true,
